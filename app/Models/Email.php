36b78a21d3c86af37136ef60dfd51e99f47098f9
Alessandro D'Orazio <alessandrodorazio@Alessandros-MacBook-Pro.local>
2021-07-02 11:53:48 +0200

    fix replace subject’s variables

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -9,78 +9,82 @@ use Orchid\Filters\Filterable;
 use Orchid\Screen\AsSource;
 use ReflectionObject;
 
-class Email extends Model {
+class Email extends Model
+{
     use AsSource, Filterable;
-
+    
     protected $table = 'email_history';
     protected $fillable = [
-        'recipient',
-        'template_id',
+        'recipient', 'template_id',
     ];
     protected $allowedSorts = [
-        'recipient',
-        'created_at',
+        'recipient', 'created_at',
     ];
     protected $hidden = [];
-
-    public function setRecipient($recipient) {
+    
+    public function setRecipient($recipient)
+    {
         $this->recipient = $recipient;
     }
-
-    public function setTemplateId($templateId) {
+    
+    public function setTemplateId($templateId)
+    {
         $this->template_id = $templateId;
     }
-
-    public function template() {
+    
+    public function template()
+    {
         return $this->belongsTo(Template::class, 'template_id', 'id');
     }
-
-    public function sendEmail($variables) {
-        $content = $this->constructEmailContent($variables);
+    
+    public function sendEmail($variables)
+    {
+        $content     = $this->constructEmailContent($variables);
         $attachments = $this->template->attachments()->get();
-        $subject = self::replaceVariablesWithContent($this->template->subject, $variables, []);
-
-        if( config('app.debug') ) {
-            Mail::to(config('mail.mailers.smtp.username'))
-                ->send(new MailDefault($subject, $content, $attachments));
+        $subject     = self::replaceVariablesWithContent($this->template->subject, $variables, []);
+        
+        if (config('app.debug')) {
+            Mail::to(config('mail.mailers.smtp.username'))->send(new MailDefault($subject, $content, $attachments));
             $this->save();
-
+            
             return;
         }
-
-        Mail::to($this->recipient)->send(new MailDefault($this->template->subject, $content, $attachments));
+        
+        Mail::to($this->recipient)->send(new MailDefault($subject, $content, $attachments));
         $this->save();
     }
-
-    public function constructEmailContent($variables, $options = []) {
-        if( $variables === null ) {
-            $variables = (object)[];
+    
+    public function constructEmailContent($variables, $options = [])
+    {
+        if ($variables === null) {
+            $variables = (object) [];
         }
-
-        $header = $this->template->layout->header;
-        $footer = $this->template->layout->footer;
+        
+        $header  = $this->template->layout->header;
+        $footer  = $this->template->layout->footer;
         $content =
-            '<mjml><mj-head>' . $this->template->layout->head . '</mj-head><mj-body>' .
-            $header . $this->template->content . $footer . '</mj-body></mjml>';
-
+            '<mjml><mj-head>'.$this->template->layout->head.'</mj-head><mj-body>'.$header.$this->template->content.
+            $footer.'</mj-body></mjml>';
+        
         return self::replaceVariablesWithContent($content, $variables, $options);
     }
-
-    public static function replaceVariablesWithContent($content, $variables, $options) {
-        if( array_key_exists("preview", $options) ) {
+    
+    public static function replaceVariablesWithContent($content, $variables, $options)
+    {
+        if (array_key_exists("preview", $options)) {
             $content =
                 str_replace('{{$', '<span style="background-color: #007bff; color: white; padding: 3px;">', $content);
             $content =
                 str_replace('{{', '<span style="background-color: #007bff; color: white; padding: 3px;">', $content);
-
+            
             $content = str_replace('}}', '</span>', $content);
         }
         $ref = new ReflectionObject($variables);
-        foreach( $ref->getProperties() as $prop ) {
-            $content = str_replace('{{$' . $prop->getName() . '}}', $prop->getValue($variables), $content);
-            $content = str_replace('{{' . $prop->getName() . '}}', $prop->getValue($variables), $content);
+        foreach ($ref->getProperties() as $prop) {
+            $content = str_replace('{{$'.$prop->getName().'}}', $prop->getValue($variables), $content);
+            $content = str_replace('{{'.$prop->getName().'}}', $prop->getValue($variables), $content);
         }
-
+        
         return $content;
     }
 }
