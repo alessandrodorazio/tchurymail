3a51b9d6da2b3f70e9c85751f9465a32caea3c3e
Alessandro D'Orazio <alessandrodorazio@Alessandros-MacBook-Pro.local>
2021-09-02 10:39:12 +0200

    typo fix

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -35,7 +35,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
                 //
-                env('horizon_allowed_user')
+                env('HORIZON_ALLOWED_USER')
             ]);
         });
     }
